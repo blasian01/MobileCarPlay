5d5cacd6b0da4e3a2f28441e04ad66a32dd437fb
blasian01 <67053548+blasian01@users.noreply.github.com>
2023-03-09 03:05:29 -0600

    Update ContentView.swift

--- MobileCarPlay/ContentView.swift
+++ MobileCarPlay/ContentView.swift
@@ -3,19 +3,32 @@
 //  MobileCarPlay
 //
 //  Created by Bronson Woods on 3/9/23.
-//
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable{
+    case 🧍🏽‍, 🙈, 🤌🏽, 😤
+}
+
 struct ContentView: View {
+    @State var selection: Emoji = .🧍🏽‍
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) {emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emojis")
+            .padding()
         }
-        .padding()
     }
 }
 
