ae142ebeca17b569e64015318a06ba1be978fd51
blasian01 <67053548+blasian01@users.noreply.github.com>
2023-03-09 02:32:31 -0600

    Initial Commit

---
+++ MobileCarPlay/MobileCarPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileCarPlayApp.swift
+//  MobileCarPlay
+//
+//  Created by Bronson Woods on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MobileCarPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
